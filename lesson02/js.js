de24496172346097262c7c38cc4b9c5ac4ce99fc
hreniks <sbigor09@gmail.com>
2020-10-10 01:50:59 +0300

    corrected 3 task

--- lesson02/js.js
+++ lesson02/js.js
@@ -1,12 +1,20 @@
-let num = 266219;
+const num = 266219;
 arr = num.toString().split('');
-let result = parseInt(arr[0],10);
+let result = 1;
+//let result = parseInt(arr[0],10);
+// for (let  i = 1;  i < arr.length;  i++){
+   
+//     result *= arr[i];
 
-for (let  i = 1;  i < arr.length;  i++) result *= arr[i];
-console.log("Result: ", result);
+// }
+arr.forEach(function(item){ result *= item});
+console.log('result: ', result);
 
-let power = result ** 3;
+result = arr.reduce(function (op,current){ return op * current});
+console.log('result: ', result);
+
+const power = result ** 3;
 console.log('Power: ', power);
 
-console.log("First 2 items: ",power.toString().substr(0,2));
+console.log("First 2 items: ", power.toString().substr(0,2));
 
