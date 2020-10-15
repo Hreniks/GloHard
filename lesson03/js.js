9645ca2dc1448fb60e038208313a7dcc94567fa6
hreniks <sbigor09@gmail.com>
2020-10-15 23:41:07 +0300

    fixed arrays

--- lesson03/js.js
+++ lesson03/js.js
@@ -1,22 +1,27 @@
 const lang = prompt('Введите значение переменной lang');
-
-if (lang === 'ru') console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
-else if (lang === 'en') console.log("Monday, Tuesday, Wednsday, Thursday, Friday, Saturday, Sunday");
-else alert('Неверное значение');
-
-switch (lang)
-{
-    case 'ru': console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
-    break;
-    case 'en': console.log("Monday, Tuesday, Wednsday, Thursday, Friday, Saturday, Sunday");
-    break;
-    default: alert('Неверное значение');
-}
-
 const array = [
     ["ru", ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]],
     ["en", ["Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday", "Sunday"]]
 ];
+const Week = [
+   ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+    ["Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday", "Sunday"]];
+
+
+if (lang === 'ru') console.log(Week[0]);
+else if (lang === 'en') console.log(Week[1]);
+else alert('Неверное значение');
+
+switch (lang)
+{
+    case 'ru': console.log(Week[0]);
+    break;
+    case 'en': console.log(Week[1]);
+    break;
+    default: alert('Неверное значение');
+}
+
+
 
 
 // lang === 'ru' ? console.log(array[0]) :
@@ -30,5 +35,5 @@ console.log(map.get(lang));
 const namePerson = prompt('Введите имя: ');
 
 namePerson === 'Артем' ? console.log('директор') : 
-namePerson === 'Максим' ? console.log('преподаватель') :
+namePerson === 'Максим' ? console.log('преподаватель') : 
 console.log('студент');
